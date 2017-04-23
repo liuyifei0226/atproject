23e1bd8f5405c110442e2ae01f026f4211228b1c
liuyifei0226 <liuyifei0226@Gmail.com>
2017-04-23 17:22:08 -0500

    Delete test2.py

--- test2.py
+++
@@ -1,7 +0,0 @@
-import atproject.Download.main as at
-
-obj = at.CustomLibrary()
-
-obj.torrent_path = "71e1c9692c556e252aa7e2f4715c419ee447039b.torrent"
-path_to_file = obj.get()
-print path_to_file
